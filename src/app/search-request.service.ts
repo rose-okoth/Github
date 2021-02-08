c47a1eb83f3893818718663f3b37f3298fe2817f
rose-okoth <okoth.rose0@gmail.com>
2021-02-08 13:13:00 +0300

    add in the github icon on the navbar

--- src/app/search-request.service.ts
+++ src/app/search-request.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import {environment} from '../environments/environment';
-import {Repository} from './repository';
-import {User} from './user';
-import { HttpClient} from '@angular/common/http';
+import { environment } from '../environments/environment';
+import { Repository } from './repository';
+import { User } from './user';
+import { HttpClient } from '@angular/common/http';
 
 
 @Injectable({
